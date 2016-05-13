44958280a96ab8c47b069f106d191a527efa8ba0
Yang <liyang9249@gmail.com>
2016-05-13 13:38:02 -0400

    remove 348

--- 348/temp.sql
+++
@@ -1,7 +0,0 @@
-SELECT tt.cno, tt.term, COUNT(*)		
-FROM			   	 (SELECT		cno, term, section, AVG(mark) AS avg_mark1
-						  FROM			enrollment
-						  WHERE term LIKE '_12'
-						  GROUP BY		cno, term, section) tt
-GROUP BY tt.term, tt.cno
-						 ;
